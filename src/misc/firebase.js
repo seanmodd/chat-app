12dc4f9fc79f85500369619d7123be8959d46c14
Sean <sean@vestaridefair.com>
2020-12-26 23:47:24 -0800

    done with firebase setup thank god

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyAdDO_Ic0msEL8xRzlzf5Gc4lP_S-cZ3Fc',
+  authDomain: 'chat-web-app-6c329.firebaseapp.com',
+  projectId: 'chat-web-app-6c329',
+  storageBucket: 'chat-web-app-6c329.appspot.com',
+  messagingSenderId: '25990469143',
+  appId: '1:25990469143:web:526a9c16e8c216526fae52',
+};
+
+const app = firebase.initializeApp(config);
