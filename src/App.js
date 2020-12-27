f13add1b549b153a0c07440baba2acde684c5e35
Sean <sean@vestaridefair.com>
2020-12-27 14:50:46 -0800

    routing in progress 12272020 2:50pm

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import 'rsuite/dist/styles/rsuite-default.css';
+import PrivateRoute from './components/PrivateRoute';
 import './styles/main.scss';
 
 function App() {
-  return <div>hello</div>;
+  return (
+    <Switch>
+      <Route>
+        <SignIn path="/signin" />
+      </Route>
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
+    </Switch>
+  );
 }
 
 export default App;
